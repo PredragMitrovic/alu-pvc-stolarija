2b8bb40e24d3f46c06f978081be09d510488a26c
Predrag Mitrovic <p.mitrovic@ilexius.de>
2025-04-14 13:37:37 +0200

    vrzija sa mapom koja radi preko typoscripta bez templejte

---
+++ packages/site_package/Configuration/TCA/Overrides/elementi/MapContactForm.php
@@ -0,0 +1,32 @@
+<?php
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'Map with Contact Form PEKI',
+        'map_contact_form',
+        'content-form'
+    ],
+    'text',
+    'after'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['map_contact_form'] = [
+    'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            --palette--;;headers,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+            categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+    ',
+];
+
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['map_contact_form'] = 'content-form';
