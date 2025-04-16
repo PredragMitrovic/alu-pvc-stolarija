c4219c29ef9b14c55e9f06a36927785477357cf1
Predrag Mitrovic <p.mitrovic@ilexius.de>
2025-04-16 08:02:18 +0200

    dodati prevodi za polja za formu

--- packages/site_package/ext_localconf.php
+++ packages/site_package/ext_localconf.php
@@ -10,3 +10,6 @@ defined('TYPO3') or die();
 
 // kažeš EXT:news‑u da proširi svoj News‑model
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News']['site_package'] = 'site_package';
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/locallang.xlf'][] =
+    'EXT:site_package/Resources/Private/Language/locallang.xlf';
